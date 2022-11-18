587f1ab9d3a14e80f9b65f685d50d5ebc3004c8c
Maorid <12201510@bsi.ac.id>
2022-11-18 19:54:29 +0700

    add login screen and sign up screnn

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.grey,
+        primarySwatch: Colors.purple,
       ),
     );
   }
